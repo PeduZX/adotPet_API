511175e79a03653bdbf96c1bee5acb042bf2c987
Pedro Farenzena <pedrofm2008@gmail.com>
2026-06-24 18:36:48 -0300

    começanco a usar o typeORM

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,14 +1,32 @@
 // classe para colocar metodos do controller
 import { Request, Response } from "express";
 import type TipoPet from "../types/TipoPet";
+import EnumEspecie from "../enum/EnumEspecie";
 
+
+let id = 0;
+function geraId() {
+  id = id + 1;
+  return id;
+}
 
 // esse é o cria pet
 let listaDePets: Array<TipoPet> = [];
 export default class PetController {
+
     criaPet(req: Request, res: Response) {
-        const { id, adotado, especie, idade, nome } = <TipoPet>req.body;
-        const novoPet: TipoPet = { id, adotado, especie, idade, nome };
+        const { adotado, especie, dataDeNacimento, nome } = <TipoPet>req.body;
+    
+        
+        if(!especie || !dataDeNacimento || !nome){
+            return res.status(400).json({erro: "preencha todos os campos"})
+        }
+
+        if(!Object.values(EnumEspecie).includes(especie)){ // ou .includes(especie as EnumEspecie)
+            return res.status(400).json({erro: "Especie inválida" })
+        }
+
+        const novoPet: TipoPet = { id: geraId(), adotado, especie, dataDeNacimento, nome };
         listaDePets.push(novoPet);
         return res.status(201).json(novoPet);
     }
@@ -19,14 +37,14 @@ export default class PetController {
 
     atualizarPet(req: Request, res: Response){
         const { id } = req.params;
-        const {adotado, especie, idade, nome} = <TipoPet>req.body
+        const {adotado, especie, dataDeNacimento, nome} = <TipoPet>req.body
         const pet = listaDePets.find((pet) => pet.id === Number(id));
         if(!pet){
             return res.status(404).json({erro: "Pet não encontrado"});
         }
 
         pet.nome = nome;
-        pet.idade = idade;
+        pet.dataDeNacimento = dataDeNacimento;
         pet.especie = especie;
         pet.adotado = adotado
         return res.status(200).json(pet)
---
+++ src/enum/EnumEspecie.ts
@@ -0,0 +1,9 @@
+// caso tenha outras especies é necessario adicionar aqui ao inves dql IF
+// cada especie que adicionar só seguir a sintaxe
+enum EnumEspecie{
+    cachorro = "cachorro",
+    gato = "gato",
+    tartaruga = "tartaruga"
+}
+
+export default EnumEspecie
